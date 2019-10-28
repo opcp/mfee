d4d44f2d9735f1fbf9bf99f6ab6221afc0e351f3
Shan-Yi <a24215188@gmail.com>
2019-10-29 00:56:08 +0800

    1029

--- src/pages/Reviews.js
+++ src/pages/Reviews.js
@@ -2,27 +2,37 @@ import React from 'react'
 import styled from '@emotion/styled'
 import '../Reviews.css'
 
-const InterBar = styled.nav`
-    border:1px solid #000;
-    height:30px;
-    width:1920px;
-    background: #aec;
-`
 const CategoryBar = styled.div`
     display:flex;
     justify-content:center;
-    width:1920px;
 `
 
-const BookImage = styled.section`
-  margin: 0 auto;
-  width:1800px;
+// 書本外框
+const Book = styled.section`
+  display:flex;
+  border-bottom:2px solid #ccc;
+  margin: 5px 2rem;
   height: 300px;
-  background: #aec;
-  border:1px solid #000
-  
+  align-items: center;
+  justify-content:space-between;
+  flex-grow:1;
 `
-
+//書本圖片
+const BookImage = styled.img`
+    padding:0 0 0 30px;
+    width:250px;
+    height:250px;
+`
+//書本資訊
+const BookInfo = styled.img`
+    width:800px;
+    height:250px;
+`
+//書本星數
+const BookStar = styled.img`
+    width:400px;
+    height:250px;
+`
 const Reviewer = () => {
     return (
         <>
@@ -35,6 +45,9 @@ const Reviewer = () => {
                 <button className="btn">文學小說</button>
                 <button className="btn">文學小說</button>
                 <button className="btn">文學小說</button>
+            </CategoryBar>
+            <CategoryBar>
+                <button className="btn">文學小說</button>
                 <button className="btn">文學小說</button>
                 <button className="btn">文學小說</button>
                 <button className="btn">文學小說</button>
@@ -43,6 +56,16 @@ const Reviewer = () => {
                 <button className="btn">文學小說</button>
                 <button className="btn">文學小說</button>
             </CategoryBar>
+            <Book>
+                <BookImage/>
+                <BookInfo/>
+                <BookStar/>
+            </Book>
+            <Book>
+                <BookImage/>
+                <BookInfo/>
+                <BookStar/>
+            </Book>
         </>
     )
 }
