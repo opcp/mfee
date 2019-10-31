23b13d90c98d9febf69951fbc250c84063414c5e
Shan Yi <a24215188@gmail.com>
2019-10-31 18:10:51 +0800

    1031

--- src/pages/Reviews.js
+++ src/pages/Reviews.js
@@ -13,7 +13,7 @@ const Main = styled.section`
 //類別欄外框
 const CategoryBar = styled.div`
   display: flex;
-  flex-wrap:wrap;
+  flex-wrap: wrap;
 `
 //右上排列方式欄位
 const OptionBar = styled.div`
@@ -26,104 +26,116 @@ const Book = styled.div`
   display: flex;
   margin: 5px 0;
   justify-content: center;
-  height: 300px;
   align-items: center;
   flex-grow: 1;
+  flex-direction: column;
 `
 //書本圖片
-const BookImage = styled.img`
-  width: 250px;
-  height: 250px;
+const BookImage = styled.div`
+  margin: 0 auto;
 `
 //書本資訊
 const BookInfo = styled.div`
-  width: 800px;
-  height: 250px;
-  border: 1px solid #ccc;
+  width: 700px;
+  margin: 0 0 40px 0;
+  ${'' /* overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 4;
+  -webkit-box-orient: vertical; */}
 `
 //書本星數
 const BookStar = styled.div`
-  width: 400px;
+  width: 350px;
   height: 250px;
   border: 1px solid #ccc;
 `
 // ------------------------------------------------------------------------------------
 
 const Reviewer = () => {
+  const [category, setCategory] = useState([])
+  const [bookinfo, setbookinfo] = useState([])
 
-    const [category, setCategory] = useState([])
-    const [bookinfo, setbookinfo] = useState([])
+  useEffect(() => {
+    categoryBar()
+    bookInfo()
+  }, [])
 
-    useEffect(() => {
-        categoryBar()
-        bookInfo()
-    }, [])
+  const categoryBar = () => {
+    axios
+      .post('http://localhost:4000/categoryBar')
+      .then(res => {
+        setCategory(res.data.data)
+        console.log(res.data.data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
 
+  const bookInfo = () => {
+    axios
+      .get('http://localhost:4000/bookInfo')
+      .then(res => {
+        setbookinfo(res.data.rows)
+        console.log(res)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+  console.log(bookinfo)
+  // category.filter()
 
-    const categoryBar = () => {
-        axios
-            .post('http://localhost:4000/categoryBar')
-            .then(res => {
-                setCategory(res.data.data)
-                console.log(res.data.data)
-            })
-            .catch(error => {
-                console.log(error)
-            })
-    }
+  return (
+    <>
+      <Router>
+        <Main>
+          <CategoryBar>
+            {category.map(data => (
+              <button key={data.sid} className="btn">
+                {data.name}
+              </button>
+            ))}
+          </CategoryBar>
+          <OptionBar>
+            <select name="array">
+              <option value="1">討論度(高>低)</option>
+              <option value="2">上市日期(新>舊)</option>
+              <option value="2">暢銷度</option>
+            </select>
+          </OptionBar>
+          <Book>
+            {/* {bookinfo.map(data => {
+              <img src={require('./BookReview/images/{data.pic}')}></img>
+            })} */}
 
-    const bookInfo = () => {
-        axios.post('http://localhost:4000/bookInfo')
-            .then(res => {
-                setbookinfo(res.data.data)
-                console.log(res.data.data)
-            })
-            .catch(error => {
-                console.log(error)
-            })
-    }
+            {bookinfo.map(data => (
+              <BookInfo key={data.sid}>
+                <h3 dangerouslySetInnerHTML={{ __html: data.name }}></h3>
+                {'作者:'}<div dangerouslySetInnerHTML={{ __html: data.author }}></div>
+                <br />
+                <br />
+                {'內容簡介:'}
+                <div>
+                  {data.detailData}
+                </div>
+              </BookInfo>
+            ))}
+            {/* <BookStar /> */}
+          </Book>
+          <ul className="pagination">
+            <li className="paginationNum">1</li>
+          </ul>
+        </Main>
 
-    return (
-        <>
-            <Router>
-                <Main>
-                    <CategoryBar>
-                        {category.map(data => (
-                            <button className="btn">{data.name}</button>
-                        ))}
-                    </CategoryBar>
-                    <OptionBar>
-                        <select name="array">
-                            <option value="1">討論度(高>低)</option>
-                            <option value="2">上市日期(新>舊)</option>
-                            <option value="2">暢銷度</option>
-                        </select>
-                    </OptionBar>
-                        <Book>
-                            <BookImage />
-                            <BookInfo>{bookinfo.map(data => (
-                                <div>{data.introduction}</div>
-                            ))}</BookInfo>
-                            <BookStar />
-                        </Book>
-                    <ul className="pagination">
-                        <li className="paginationNum">1</li>
-                        <li className="paginationNum">2</li>
-                        <li className="paginationNum">3</li>
-                        <li className="paginationNum">4</li>
-                        <li className="paginationNum">5</li>
-                        <li className="paginationNum">6</li>
-                        <li className="paginationNum">7</li>
-                        <li className="paginationNum">8</li>
-                    </ul>
-                </Main>
-
-                <Switch>
-                    <Route path="http://localhost:3000/list/" />
-                </Switch>
-            </Router>
-        </>
-    )
+        <Switch>
+          <Route path="http://localhost:3000/List/" />
+        </Switch>
+      </Router>
+    </>
+  )
 }
 
 export default Reviewer
